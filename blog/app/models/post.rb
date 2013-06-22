19551bba6cd51e8d34f2da5740361e753b5cfaf6
baichuan <biorainy@gmail.com>
2013-06-21 23:01:54 -0400

    Revert "created blog project"
    This reverts commit 2d2e260ff8ff5e25555194f3bff323fc9557cbf2.

--- blog/app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ActiveRecord::Base
-  has_many :comments, :dependent => :destroy;
-  validates_uniqueness_of :title                         
-  validates_presence_of :title, :content, :user   
-end
--- blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '2c87d0e3a394e87148860db9c293b7a3f66806c0c7c9870f65cb5b6f1b1405589ba78388df55d7da96cf85eba7ad9d801a994e5ab05a38599bd8af2c049b7b05'
