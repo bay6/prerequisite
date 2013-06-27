2dc74d17d59950e406b7c4c701b8a61caa7e9a50
zhangyi2099 <zhangyi2099@gmail.com>
2013-06-27 21:16:49 +0800

    add image uploader

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments, :dependent => :destroy;
   validates_uniqueness_of :title                         
-  validates_presence_of :title, :content, :user   
+  validates_presence_of :title, :content, :user 
+  #carrierwave image uploader
+  mount_uploader :image,ImageUploader  
 end
---
+++ blog/db/migrate/20130627131037_add_image_to_posts.rb
@@ -0,0 +1,6 @@
+class AddImageToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :image, :string
+
+  end
+end
