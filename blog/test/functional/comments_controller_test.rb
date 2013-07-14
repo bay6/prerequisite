3441d7e98f75c37f859d75121b945cd171b53dec
txz <281040844@qq.com>
2013-07-15 01:01:38 +0800

    Update comments_controller_test.rb
    test

--- blog/test/functional/comments_controller_test.rb
+++ blog/test/functional/comments_controller_test.rb
@@ -17,33 +17,33 @@ class CommentsControllerTest < ActionController::TestCase
   end
 
   test "should create comment" do
-    assert_difference('Comment.count') do
-      post :create, :comment => @comment.attributes
-    end
-
-    assert_redirected_to comment_path(assigns(:comment))
-  end
-
-  test "should show comment" do
-    get :show, :id => @comment
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @comment
-    assert_response :success
-  end
-
-  test "should update comment" do
-    put :update, :id => @comment, :comment => @comment.attributes
-    assert_redirected_to comment_path(assigns(:comment))
-  end
-
-  test "should destroy comment" do
-    assert_difference('Comment.count', -1) do
-      delete :destroy, :id => @comment
-    end
-
-    assert_redirected_to comments_path
+     assert_difference('Comment.count') do
+       post :create, comment: { author: @comment.author, email: @comment.email, message: @comment.message }
+     end
+ 
+     assert_redirected_to comment_path(assigns(:comment))
+   end
+ 
+   test "should show comment" do
+     get :show, id: @comment
+     assert_response :success
+   end
+ 
+   test "should get edit" do
+     get :edit, id: @comment
+     assert_response :success
+   end
+ 
+   test "should update comment" do
+     put :update, id: @comment, comment: { author: @comment.author, email: @comment.email, message: @comment.message }
+     assert_redirected_to comment_path(assigns(:comment))
+   end
+ 
+   test "should destroy comment" do
+     assert_difference('Comment.count', -1) do
+      delete :destroy, id: @comment
+     end
+ 
+     assert_redirected_to comments_path
   end
 end
